cf65c2089daee9324419560f506a7cb809046d12
Bill-1991 <altzebill@yahoo.com>
2023-02-28 16:13:14 +0200

    fix: tailwind-issue

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}",
-            "./**/*.html"],
+  content: ["./src/**/*.js",
+            "./index.html"],
   theme: {
     extend: {},
   },
