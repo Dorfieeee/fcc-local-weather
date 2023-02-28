fa249b685e70352db6955100e16977bd6273c24c
Bill-Altze <107632771+Bill-1991@users.noreply.github.com>
2023-02-28 16:31:41 +0200

    fix: tailwind-issue (#5)
    * fix: tailwind-issue
    
    * fix: tailwind-issue

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.js",
+            "./index.html"],
   theme: {
     extend: {},
   },
