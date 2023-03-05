e2fb68a47ee85b510f7d5fd04f8393bef515369a
David Rosendorf <rosendorf.dev@gmail.com>
2023-03-05 21:22:40 +0000

    utilize more data from api

--- src/updateWeather.js
+++ src/updateWeather.js
@@ -1,5 +1,5 @@
 import getWeatherDetails from "./apis/weatherDetails.js";
-import { $ } from "./utils.js";
+import { $, epochToTime } from "./utils.js";
 
 /**
  * Updates the weather information with the most recent data
@@ -13,20 +13,33 @@ export default async (lat, lon) => {
   const $city = $("#city");
   const $temperature = $("#temp");
   const $icon = $("#icon");
+  const $humidity = $("#humidity");
+  const $wind = $("#wind");
+  const $pressure = $("#pressure");
+  const $sun = $("#sunrise-sunset");
+  const $feelsLike = $("#feels-like");
+  const $visibility = $("#visibility");
 
   const weatherDetails = await getWeatherDetails(lat, lon);
-  const weather = weatherDetails.weather[0];
 
-  console.log(weatherDetails);
+  const { country, sunset, sunrise } = weatherDetails.sys;
+  const { name, dt, visibility } = weatherDetails;
+  const { feels_like, humidity, pressure, temp, temp_max, temp_min } =
+    weatherDetails.main;
+  const { icon, description } = weatherDetails.weather[0];
+  const { speed, deg } = weatherDetails.wind;
 
-  const country = weatherDetails.sys.country;
-  const cityName = weatherDetails.name;
-  const temp = weatherDetails.main.temp;
-  const iconUrl = weather.icon;
-
-  $city.innerHTML = `${cityName}, ${country}`;
-  $temperature.innerHTML = `${temp} &#176;C`;
+  $visibility.innerHTML = `${Math.floor(visibility / 1000)} km`;
+  $city.innerHTML = `${name}, ${country}`;
+  $humidity.innerHTML = `${humidity} %`;
+  $wind.innerHTML = `${speed} km/h`;
+  $pressure.innerHTML = `${pressure} hPa`;
+  $sun.innerHTML = ` &uarr;${epochToTime(sunrise)}<br>&darr;${epochToTime(
+    sunset
+  )}`;
+  $feelsLike.innerHTML = `${Math.round(feels_like)} &#176;C`;
+  $temperature.innerHTML = `${Math.round(temp)} &#176;C`;
   $icon.innerHTML = `
-        <img src=${iconUrl} alt="${weather.description}" width=120 height=120 />
+        <img src=${icon} alt="${description}" width=120 height=120 />
       `;
 };
