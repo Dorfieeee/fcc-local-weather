52194d923d12cecf8a7eb29404cb22c612c76033
David Rosendorf <rosendorf.dev@gmail.com>
2023-03-10 10:45:05 +0000

    update utils.js

--- src/utils.js
+++ src/utils.js
@@ -6,3 +6,19 @@ export const epochToTime = (seconds) =>
   new Intl.DateTimeFormat(navigator.language, { timeStyle: "short" }).format(
     new Date(seconds * 1000)
   );
+
+/**
+ * Converts a value representing Celcius to Fahrenheit
+ * @param {float} C
+ * @returns {float}
+ */
+export const celciusToFahrenheit = (C) => C * (9 / 5) + 32;
+
+/**
+ * Converts a value representing Fahrenheit to Celcius
+ * @param {float} F
+ * @returns {float}
+ */
+export const fahrenheitToCelcius = (F) => (F - 32) * (5 / 9);
+
+export const kmToMi = (km) => km / 1.609344;
