a1c00278fd150237f85f7a2eeb9f1cb344215a05
hcampos <88628151+Sakoutecher@users.noreply.github.com>
2023-03-17 10:21:59 +0100

    style: fonts (#22)
    * Add google fonts links
    
    * Config the new fonts in the tailwing config file
    
    * Apply new fonts on text in the index.html
    
    * Remove font-paragraph and replace it by font-global
    
    * Remove all font-paragraph and the Jost font on all the index, put the font on the body

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/**/*.{js, html}", "./index.html"],
   theme: {
     extend: {
+      fontFamily: {
+        title: ['"Tilt Warp"', "cursive"],
+        global: ['"Jost"', "cursive"],
+      },
       gridTemplateRows: {
         // Set the first line to 6rem(navigation, search), rest auto
         layout: "6rem auto",
