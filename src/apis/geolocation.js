40c5e64dfb4c3e6ea402e79ad3ed753b9671f0a3
Bill-1991 <altzebill@yahoo.com>
2023-03-17 22:07:39 +0200

    show weather details on user search

---
+++ src/apis/geolocation.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export default async (city, stateCode, countryCode) => {
+  try {
+    let coords = [];
+    const response = await axios.get(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${city}
+            ,${stateCode},${countryCode}&limit=1&appid=${
+        import.meta.env.VITE_API_KEY
+      }`
+    );
+    coords[0] = response.data[0].lat;
+    coords[1] = response.data[0].lon;
+    return coords;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import "./style.css";
 import updateWeather from "./updateWeather";
 import getWeatherDetails from "./apis/weatherDetails.js";
+import getLocationCoords from "./apis/geolocation.js";
 import { $ } from "./utils";
 
 let weatherData = null;
@@ -31,6 +32,28 @@ function handleUnitSwitchClick(e) {
   updateWeather(weatherData, unitSystem);
 }
 
+async function handleDisplayWeatherBySearch() {
+  let searchText = document.getElementById("string").value;
+  let splitText = searchText.split(",");
+  let city = "";
+  let stateCode = "";
+  let countryCode = "";
+  let coords;
+  if (splitText.length < 3) {
+    city = splitText[0].trim();
+    countryCode = splitText[1].trim();
+    coords = await getLocationCoords(city, countryCode);
+  } else {
+    city = splitText[0].trim();
+    stateCode = splitText[1].trim();
+    countryCode = splitText[2].trim();
+    coords = await getLocationCoords(city, stateCode, countryCode);
+  }
+
+  weatherData = await getWeatherDetails(coords[0], coords[1]);
+  updateWeather(weatherData, unitSystem);
+}
+
 function main() {
   // ask the user for a permission to access his geolocation information
   navigator.geolocation.getCurrentPosition(
@@ -40,4 +63,5 @@ function main() {
 
   $("#celsiusBtn").addEventListener("click", handleUnitSwitchClick);
   $("#fahrenheitBtn").addEventListener("click", handleUnitSwitchClick);
+  $("#submit").addEventListener("click", handleDisplayWeatherBySearch);
 }
