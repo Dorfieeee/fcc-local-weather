d6b98fb1b3906b61a067f3b9a63038b6ef4cd6e2
David Rosendorf <rosendorf.dev@gmail.com>
2023-04-22 20:35:19 +0100

    feat: forecast slider (#30)

--- src/main.js
+++ src/main.js
@@ -36,12 +36,12 @@ async function handleRejectPermission() {
   updateWeather(weatherData, unitSystem);
 }
 
-function handleUnitSwitchClick(e) {
-  let unitSelected = e.target.value;
+function handleUnitSwitchClick(event) {
+  let unitSelected = event.target.value;
 
   if (unitSystem === unitSelected) return;
 
-  for (let btn of e.target.parentElement.children) {
+  for (let btn of event.target.parentElement.children) {
     toggleUnitSwitchStyle(btn);
   }
 
@@ -53,6 +53,46 @@ function toggleUnitSwitchStyle(el) {
   el.classList.toggle("active");
 }
 
+const handleForecastPointerDown = (() => {
+  let translateX = 0;
+
+  return (event) => {
+    event.preventDefault();
+
+    const $forecast = event.currentTarget;
+    const $forecastContainer = event.currentTarget.parentElement;
+    let forecastMaxOffset =
+      $forecast.getBoundingClientRect().width -
+      $forecastContainer.getBoundingClientRect().width;
+
+    $forecast.setPointerCapture(event.pointerId);
+
+    $forecast.style.cursor = "grabbing";
+
+    $forecast.addEventListener("pointermove", handlePointerMoveOverForecast);
+    $forecast.addEventListener("pointerup", handlePointerUpForecast);
+
+    function handlePointerMoveOverForecast(event) {
+      translateX += event.movementX;
+      if (forecastMaxOffset + translateX < 0) {
+        translateX = -forecastMaxOffset;
+      } else if (translateX > 0) {
+        translateX = 0;
+      }
+      $forecast.style.transform = `translateX(${translateX}px)`;
+    }
+
+    function handlePointerUpForecast(e) {
+      $forecast.style.cursor = "";
+      $forecast.removeEventListener(
+        "pointermove",
+        handlePointerMoveOverForecast
+      );
+      $forecast.removeEventListener("pointerup", handlePointerUpForecast);
+    }
+  };
+})();
+
 function main() {
   // ask the user for a permission to access his geolocation information
   navigator.geolocation.getCurrentPosition(
@@ -62,6 +102,8 @@ function main() {
 
   $("#celsiusBtn").addEventListener("click", handleUnitSwitchClick);
   $("#fahrenheitBtn").addEventListener("click", handleUnitSwitchClick);
+  $("#forecast").addEventListener("pointerdown", handleForecastPointerDown);
+  $("#forecast").addEventListener("dragstart", () => false);
 
   if (unitSystem === "metric") {
     toggleUnitSwitchStyle($("#celsiusBtn"));
